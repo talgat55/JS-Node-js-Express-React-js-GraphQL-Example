4be4ee0b000acd507d8c63b52dbc8aae793612b0
talgat55 <lightxdesign55@gmail.com>
2020-01-11 02:02:55 +0600

    add server side code

---
+++ server/resolvers.js
@@ -0,0 +1,25 @@
+import Note from './models/note';
+export const resolvers = {
+
+    Query : {
+        async allNotes(){
+            return await Note.find();
+        },
+        async getNote(root, {_id}){
+            return await Note.findById(_id);
+        },
+    },
+
+    Mutation: {
+        async createNote(root, { input }) {
+            return await Note.create(input);
+        },
+        async updateNote(root, {_id, input}){
+            return await Note.findOneAndUpdate({_id},input,{new: true})
+        },
+        async deleteNote(root, {_id}){
+            return await Note.findOneAndRemove({_id});
+        }
+    }
+
+};
